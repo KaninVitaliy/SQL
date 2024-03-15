3bcbc719943a4c8409ef30d56c92abf87f964d5b
Vitaliy_Kanin <kaninv1611997@mail.ru>
2024-03-15 13:41:16 +0300

    SQL Продвинутая выборка данных, доработка

---
+++ Задание 2 Select запросы .sql
@@ -0,0 +1,23 @@
+-- 1. Получение самого длинного трека 
+SELECT track_name , duration  
+FROM track 
+WHERE duration = (SELECT MAX(duration) FROM track);
+
+-- 2. Название треков, продолжительность которых не менее 3,5 минут
+SELECT track_name 
+FROM track 
+WHERE duration >= 210;
+
+-- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно
+SELECT collection_tittle 
+FROM collection 
+WHERE year_collection >= 2018 AND year_collection <= 2020;
+
+-- 4. Исполнители, чьё имя состоит из одного слова.
+SELECT nick_name 
+FROM artist  
+WHERE nick_name NOT LIKE '% %';
+
+-- 5. Название треков, которые содержат слово «мой» или «my».
+SELECT track_name  FROM track 
+WHERE track_name LIKE '% my %' OR track_name LIKE '% мой %';
--- Задание 4 SELECT запросы.sql
+++ Задание 4 SELECT запросы.sql
@@ -1,9 +1,19 @@
+-- 1. Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT  a2.album_tittle  FROM genre g 
+JOIN genre_artist ga ON ga.genre_id = g.genre_id
+JOIN artist a ON ga.artist_id = a.artist_id 
+JOIN album_artist aa ON a.artist_id = aa.artist_id 
+JOIN album a2 ON aa.album_id = a2.album_id
+GROUP BY a2.album_id 
+HAVING COUNT(g.genre_id) > 1;
+
+
 -- 2. Наименование треков которые не входят в сборники.
 
 SELECT t.track_name FROM track t
    WHERE t.track_name NOT IN (
    SELECT DISTINCT t.track_name FROM collection c 
-   JOIN collection_track ct ON ct.collection_id = c.collection_id
+   LEFT JOIN collection_track ct ON ct.collection_id = c.collection_id
    JOIN track t ON ct.track_id = t.track_id 
    ); 
 
@@ -19,6 +29,8 @@ WHERE t.duration = (
 
 -- 4. Названия альбомов, содержащих наименьшее количество треков.
 
-SELECT DISTINCT a.album_tittle, count(t.track_name)  FROM album a
+SELECT DISTINCT a.album_tittle FROM album a
 JOIN track t ON t.album_id = a.album_id
-GROUP BY a.album_tittle ;
+GROUP BY a.album_id  
+HAVING COUNT(t.track_id) = (SELECT MIN(t.track_id) FROM album a2
+JOIN track t ON t.album_id  = a2.album_id);
